e8b16f1b58bccd0ff76e956e0bb3408eae765f78
Vidhaath07 <vidhaath007@gmail.com>
2026-03-04 18:56:19 +0530

    Add OOps class to display a banner

---
+++ OOps.java
@@ -0,0 +1,17 @@
+public class OOps {
+
+    public static void main(String[] args) {
+
+        String[] banner = {
+                String.join("  ", "OOOO", "OOOO", "PPPP", "SSSS"),
+                String.join("  ", "O  O", "O  O", "P   P", "S"),
+                String.join("  ", "O  O", "O  O", "PPPP", "SSSS"),
+                String.join("  ", "O  O", "O  O", "P", "    S"),
+                String.join("  ", "OOOO", "OOOO", "P", "SSSS")
+        };
+
+        for (String line : banner) {
+            System.out.println(line);
+        }
+    }
+}
